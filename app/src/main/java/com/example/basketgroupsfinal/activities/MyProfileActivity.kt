6cc8d9084efda4b3df4c635e4da0e6aa7ddb6af4
Herescu Vlad <vladherescu99@gmail.com>
2022-09-16 01:05:05 +0300

    update profile data

--- app/src/main/java/com/example/basketgroupsfinal/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/basketgroupsfinal/activities/MyProfileActivity.kt
@@ -1,18 +1,48 @@
 package com.example.basketgroupsfinal.activities
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.webkit.MimeTypeMap
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.basketgroupsfinal.R
 import com.example.basketgroupsfinal.databinding.ActivityMainBinding
 import com.example.basketgroupsfinal.databinding.ActivityMyProfileBinding
 import com.example.basketgroupsfinal.firebase.FirestoreClass
 import com.example.basketgroupsfinal.models.User
+import com.example.basketgroupsfinal.utils.Constants
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
 
+    companion object {
+        //A unique code for asking the Read Storage Permission using this we will be check and identify in the method onRequestPermissionsResult
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+
+    }
+
     private var binding: ActivityMyProfileBinding? = null
+
+    private var mSelectedImageFileUri: Uri? = null
+
+    private lateinit var mUserDetails: User
+
+    // A global variable for a user profile image URL
+    private var mProfileImageURL: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +53,94 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
 
         FirestoreClass().loadUserData(this@MyProfileActivity)
+
+        binding?.ivUserImage?.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO (Step 8: Call the image chooser function.)
+                // START
+                showImageChooser()
+                // END
+            } else {
+                /*Requests permissions to be granted to this application. These permissions
+                 must be requested in your manifest, they should not be granted to your app,
+                 and they should have protection level*/
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+
+        }
+
+        binding?.btnUpdate?.setOnClickListener {
+
+            // Here if the image is not selected then update the other details of user.
+            if (mSelectedImageFileUri != null) {
+
+                uploadUserImage()
+            } else {
+
+                showProgressDialog(resources.getString(R.string.please_wait))
+
+                // Call a function to update user details in the database.
+                updateUserProfileData()
+            }
+        }
+
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // TODO (Step 9: Call the image chooser function.)
+                // START
+                showImageChooser()
+                // END
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    "Oops, you just denied the permission for storage. You can also allow it from settings.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+        }
+    }
+
+    val getImage = registerForActivityResult(
+        ActivityResultContracts.GetContent(),
+        ActivityResultCallback {
+
+            mSelectedImageFileUri = it
+
+            try{
+                Glide
+                    .with(this@MyProfileActivity)
+                    .load(it)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(binding?.ivUserImage!!)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+    )
+
+    private fun showImageChooser() {
+
+        getImage.launch("image/*")
 
     }
 
@@ -44,6 +162,8 @@ class MyProfileActivity : BaseActivity() {
 
     fun setUserDataInUI(user: User) {
 
+        mUserDetails = user
+
         val ivImage = binding?.ivUserImage
 
         Glide
@@ -59,6 +179,93 @@ class MyProfileActivity : BaseActivity() {
             binding?.etMobile?.setText(user.mobile.toString())
         }
     }
+
+    private fun getFileExtension(uri: Uri?): String? {
+        /*
+         * MimeTypeMap: Two-way map that maps MIME-types to file extensions and vice versa.
+         *
+         * getSingleton(): Get the singleton instance of MimeTypeMap.
+         *
+         * getExtensionFromMimeType: Return the registered extension for the given MIME type.
+         *
+         * contentResolver.getType: Return the MIME type of the given content URL.
+         */
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(uri!!))
+    }
+
+    private fun uploadUserImage() {
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        if (mSelectedImageFileUri != null) {
+
+            //getting the storage reference
+            val sRef: StorageReference = FirebaseStorage.getInstance().reference.child(
+                "USER_IMAGE" + System.currentTimeMillis() + "." + getFileExtension(
+                    mSelectedImageFileUri
+                )
+            )
+
+            //adding the file to reference
+            sRef.putFile(mSelectedImageFileUri!!)
+                .addOnSuccessListener { taskSnapshot ->
+                    // The image upload is success
+                    Log.i(
+                        "Firebase Image URL",
+                        taskSnapshot.metadata!!.reference!!.downloadUrl.toString()
+                    )
+
+                    // Get the downloadable url from the task snapshot
+                    taskSnapshot.metadata!!.reference!!.downloadUrl
+                        .addOnSuccessListener { uri ->
+                            Log.i("Downloadable Image URL", uri.toString())
+
+                            // assign the image url to the variable.
+                            mProfileImageURL = uri.toString()
+
+                            // Call a function to update user details in the database.
+                            updateUserProfileData()
+                        }
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(
+                        this@MyProfileActivity,
+                        exception.message,
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    hideProgressDialog()
+                }
+        }
+    }
+
+    private fun updateUserProfileData() {
+        val userHashMap = HashMap<String, Any>()
+
+        if (mProfileImageURL.isNotEmpty() && mProfileImageURL != mUserDetails.image) {
+            userHashMap[Constants.IMAGE] = mProfileImageURL
+        }
+
+        if (binding?.etName?.text.toString() != mUserDetails.name) {
+            userHashMap[Constants.NAME] = binding?.etName?.text.toString()
+        }
+
+        if (binding?.etMobile?.text.toString() != mUserDetails.mobile.toString()) {
+            userHashMap[Constants.MOBILE] = binding?.etMobile?.text.toString().toLong()
+        }
+
+        // Update the data in the database.
+        FirestoreClass().updateUserProfileData(this@MyProfileActivity, userHashMap)
+
+    }
+
+    fun profileUpdateSuccess() {
+
+        hideProgressDialog()
+
+        finish()
+    }
+
 
 
 }
--- app/src/main/java/com/example/basketgroupsfinal/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/basketgroupsfinal/firebase/FirestoreClass.kt
@@ -2,6 +2,7 @@ package com.example.basketgroupsfinal.firebase
 
 import android.app.Activity
 import android.util.Log
+import android.widget.Toast
 import com.example.basketgroupsfinal.activities.MainActivity
 import com.example.basketgroupsfinal.activities.MyProfileActivity
 import com.example.basketgroupsfinal.activities.SignInActivity
@@ -102,7 +103,28 @@ class FirestoreClass {
         return currentUserID
     }
 
+    fun updateUserProfileData(activity: MyProfileActivity, userHashMap: HashMap<String, Any>) {
+        mFireStore.collection(Constants.USERS) // Collection Name
+            .document(getCurrentUserID()) // Document ID
+            .update(userHashMap) // A hashmap of fields which are to be updated.
+            .addOnSuccessListener {
+                // Profile data is updated successfully.
+                Log.i(activity.javaClass.simpleName, "Profile Data updated successfully!")
 
+                Toast.makeText(activity, "Profile updated successfully!", Toast.LENGTH_SHORT).show()
+
+                // Notify the success result.
+                activity.profileUpdateSuccess()
+            }
+            .addOnFailureListener { e ->
+                activity.hideProgressDialog()
+                Log.e(
+                    activity.javaClass.simpleName,
+                    "Error while updating user data.",
+                    e
+                )
+            }
+    }
 
 
 }
